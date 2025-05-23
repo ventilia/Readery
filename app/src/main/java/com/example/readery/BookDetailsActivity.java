82590c8b161deb5545120cc3c2d71677d5e14bf7
ventt <rfhfcm2009@gmail.com>
2025-05-23 06:23:54 +0300

    git commit git push

--- app/src/main/java/com/example/readery/BookDetailsActivity.java
+++ app/src/main/java/com/example/readery/BookDetailsActivity.java
@@ -3,6 +3,9 @@ package com.example.readery;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.text.TextPaint;
 import android.text.style.StrikethroughSpan;
 import android.view.View;
 import android.widget.TextView;
@@ -62,7 +65,28 @@ public class BookDetailsActivity extends AppCompatActivity {
             descriptionShortTextView.setText(shortDescription);
             descriptionShortTextView.setVisibility(View.VISIBLE);
             expandDescriptionTextView.setVisibility(View.VISIBLE);
-            descriptionFullTextView.setText(description);
+
+            // Полное описание с кликабельной надписью "свернуть"
+            String fullDescriptionWithCollapse = description + " свернуть";
+            SpannableString spannableDescription = new SpannableString(fullDescriptionWithCollapse);
+            ClickableSpan collapseSpan = new ClickableSpan() {
+                @Override
+                public void onClick(View widget) {
+                    descriptionShortTextView.setVisibility(View.VISIBLE);
+                    expandDescriptionTextView.setVisibility(View.VISIBLE);
+                    descriptionFullTextView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    super.updateDrawState(ds);
+                    ds.setUnderlineText(false);
+                }
+            };
+            spannableDescription.setSpan(collapseSpan, description.length() + 1, fullDescriptionWithCollapse.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannableDescription.setSpan(new ForegroundColorSpan(0xFF0000FF), description.length() + 1, fullDescriptionWithCollapse.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            descriptionFullTextView.setText(spannableDescription);
+            descriptionFullTextView.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
             descriptionFullTextView.setVisibility(View.GONE);
 
             expandDescriptionTextView.setOnClickListener(v -> {
